296fbd96792f2d6dd338963a5322da5a9630b8e5
patrictobola <tobola92@gmail.com>
2023-09-07 15:41:22 +0200

    Done functions in model to link project and tech

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -16,4 +16,8 @@ class Project extends Model
     {
         return $this->belongsTo(Type::class);
     }
+    public function technologies()
+    {
+        return $this->belongsToMany(Technology::class);
+    }
 }
